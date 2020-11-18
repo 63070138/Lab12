744f0977a889c80d6f3c68e7a23fafe0108bba9a
63070138 <73683338+63070138@users.noreply.github.com>
2020-11-18 23:30:32 +0700

    Update update.php

--- www/ITF/update.php
+++ www/ITF/update.php
@@ -1,7 +1,7 @@
 <?php
 $id=$_GET['ID'];
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'phufarpsw.mysql.database.azure.com', 'phufarpsw@phufarpsw', 'EMLbmk28', 'itflab', 3306);
+mysqli_real_connect($conn, 'putterlnwza.mysql.database.azure.com', 'it63070138@putterlnwza', 'FXFlxm58', 'itflab', 3306);
 $name=$_POST['Name'];
 $comment=$_POST['Comment'];
 $link=$_POST['Link'];
